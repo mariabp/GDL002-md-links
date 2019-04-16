d36a482d14480b78003b2104cef6dea29347cea6
Maria Bolaños <mariadlp@gmail.com>
2019-04-16 18:46:18 -0500

    Comencé a preparar los test con las funciones que requiero para obtener links validados

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,63 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../index.js ');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
+	it('should be an object ', () => {
+
+		console.log('FIX ME!');
+
+	});
 
 });
+
+describe('validatePath', () => {
+
+	it('should be a function ', () => {
+
+		console.log('absolutePath');
+
+	});
+
+});
+
+describe('filterMdFiles', () => {
+
+	it('should be a function ', () => {
+
+		console.log('true');
+
+	});
+
+});
+
+describe('getFile', () => {
+
+	it('should be a function ', () => {
+
+		console.log('file');
+
+	});
+
+});
+
+describe('getLinks', () => {
+
+	it('should be a function ', () => {
+
+		console.log('linkCollection');
+
+	});
+
+});
+
+describe('validateLinks', () => {
+
+	it('should be a function ', () => {
+
+		console.log('validatedLinks');
+
+	});
+
+});
+
+
