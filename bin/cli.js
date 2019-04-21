c2d79682bdfb17b0feeb5d378884800dfd65552f
Maria Bolaños <mariadlp@gmail.com>
2019-04-21 14:10:18 -0500

    Agregué variables para almacenar las opciones que ingrese el usuario

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,11 @@
 const mdLinks = require('../src/index');
 
 const givenPath = process.argv[2];
-const options = [process.argv[3], process.argv[4]];
+
+const optionOne = process.argv[3];
+
+const optionTwo = process.argv[4];
+
+const options = [optionOne, optionTwo];
 
 mdLinks(givenPath, options);
